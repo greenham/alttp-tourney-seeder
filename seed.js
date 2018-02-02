28e23a51118daaa5ebd87d078aeee7e790e841b4
Chris Ham <greenham@users.noreply.github.com>
2018-02-01 16:54:56 -0800

    make buckets/seeding dynamic based on groupSize

--- seed.js
+++ seed.js
@@ -21,34 +21,32 @@ participants.sort((a, b) => {
 	return a.pb - b.pb;
 });
 
-// @TODO: Make this dynamic based on groupSize
-// Segment participants into buckets of 32
-let bucket0, bucket1, bucket2, bucket3;
-bucket0 = participants.slice(0,32);
-bucket1 = participants.slice(32,64);
-bucket2 = participants.slice(64,96);
-bucket3 = participants.slice(96);
-
-// Set seed
+// Set participant's original seed
 participants.forEach((participant, index) => {
 	participant.seed = index+1;
 	participants[index] = participant;
 	//console.log(`${index+1}. ${participant.srcUsername}`);
 });
 
-// Place participants in groups of 4, one from each bucket
-let numberOfGroups = participants.length / groupSize;
-let groups = [];
-for (var i = 1; i <= numberOfGroups; i++) {
-	// Pick a random participant from each bucket to create a group
-	let newGroup = [
-		bucket0.splice(Math.floor(Math.random() * bucket0.length), 1)[0],
-		bucket1.splice(Math.floor(Math.random() * bucket1.length), 1)[0],
-		bucket2.splice(Math.floor(Math.random() * bucket2.length), 1)[0],
-		bucket3.splice(Math.floor(Math.random() * bucket3.length), 1)[0]
-	];
+// Create the appropriate number of buckets based on groupSize
+let numGroups = participants.length / groupSize;
+let buckets = [];
+for (let i = 0; i < groupSize; i++) {
+	buckets.push(participants.slice(i*numGroups,numGroups*(i+1)));
+}
 
-	console.log(`Group ${i}: ${newGroup[0].srcUsername} (${newGroup[0].seed}), ${newGroup[1].srcUsername} (${newGroup[1].seed}), ${newGroup[2].srcUsername} (${newGroup[2].seed}), ${newGroup[3].srcUsername} (${newGroup[3].seed})`);
+// Place participants in groups of groupSize, one from each bucket
+let groups = [];
+for (let i = 0; i < numGroups; i++) {
+	let newGroup = [];
+	let groupLog = [];
+	for (let j = 0; j < groupSize; j++) {
+		let p = buckets[j].splice(Math.floor(Math.random() * buckets[j].length), 1)[0];
+		newGroup.push(p);
+		groupLog.push(`${p.srcUsername} (${p.seed})`);
+	}
+	
+	console.log(`Group ${i+1}: ${groupLog.join(', ')}`);
 	groups.push(newGroup);
 }
 
